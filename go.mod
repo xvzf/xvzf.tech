2d7a1f3e16379d8bf32152cbe49946c99da998c2
Matthias Riegler <me@xvzf.tech>
2019-10-20 14:18:16 +0200

    access log

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.13
 require (
 	github.com/gobuffalo/packr v1.30.1
 	github.com/gobuffalo/packr/v2 v2.7.1 // indirect
+	github.com/gorilla/handlers v1.4.2
 	github.com/karrick/godirwalk v1.12.0 // indirect
 	github.com/rogpeppe/go-internal v1.5.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gobuffalo/packr"
+	"github.com/gorilla/handlers"
 )
 
 func main() {
 	box := packr.NewBox("./dist")
 
 	http.Handle("/", http.FileServer(box))
-	http.ListenAndServe(":3000", nil)
+	err := http.ListenAndServe(":3000", handlers.LoggingHandler(os.Stdout, http.DefaultServeMux))
+	if err != nil {
+		log.Fatal("Could not serve")
+	}
 }
