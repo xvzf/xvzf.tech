e6187240266f3a64eb86efbb5a934ac37561761d
Matthias Riegler <me@xvzf.tech>
2019-10-20 13:36:43 +0200

    initial version

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,91 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--i-container class="_height-100">
+      <i-row center-xs middle-xs class="_height-100">
+        <i-column>
+          <img src="me.jpeg" class="image">
+          <h1>Matthias Riegler</h1>
+          <p>
+            Passionate about <strong>Open Source</strong>, <strong>IPv6</strong>, <strong>Kubernetes</strong> and <strong>routing more than 10G</strong>
+          </p>
+          <i-button link variant="dark" href="https://github.com/xvzf"> <font-awesome-icon :icon="['fab', 'github-alt']" size="lg"/> </i-button>
+          <i-button link variant="dark" href="https://twitter.com/tuxoide"><font-awesome-icon :icon="['fab', 'twitter']" size="lg"/> </i-button>
+          <i-button link variant="dark" href="https://www.linkedin.com/in/matthias-riegler-364a52b5/"><font-awesome-icon :icon="['fab', 'linkedin-in']" size="lg"/> </i-button>
+          <i-button link variant="dark" href="mailto:me@xvzf.tech"><font-awesome-icon icon="envelope" size="lg"/> </i-button>
+        </i-column>
+      </i-row>
+
+      <i-row>
+        <i-column>
+          <p>test</p>
+        </i-column>
+      </i-row>
+
+    </i-container-->
+    <i-layout class="_height-100">
+      <i-layout-content>
+        <i-container class="_height-100">
+          <i-row center-xs middle-xs class="_height-100">
+            <i-column>
+              <img src="me.jpeg" class="image" />
+              <h1>Matthias Riegler</h1>
+              <p>
+                Passionate about
+                <strong>Open Source</strong>,
+                <strong>IPv6</strong>,
+                <strong>Kubernetes</strong> and
+                <strong>routing more than 10G</strong>
+              </p>
+              <i-button link variant="dark" href="https://github.com/xvzf">
+                <font-awesome-icon :icon="['fab', 'github-alt']" size="lg" />
+              </i-button>
+              <i-button link variant="dark" href="https://twitter.com/tuxoide">
+                <font-awesome-icon :icon="['fab', 'twitter']" size="lg" />
+              </i-button>
+              <i-button
+                link
+                variant="dark"
+                href="https://www.linkedin.com/in/matthias-riegler-364a52b5/"
+              >
+                <font-awesome-icon :icon="['fab', 'linkedin-in']" size="lg" />
+              </i-button>
+              <i-button link variant="dark" href="mailto:me@xvzf.tech">
+                <font-awesome-icon icon="envelope" size="lg" />
+              </i-button>
+            </i-column>
+          </i-row>
+        </i-container>
+      </i-layout-content>
+      <i-layout-footer class="footer">
+        <i-container fluid>
+          <i-row xs-center>
+            <i-column>
+              <div style="font-size: 12px;">
+                &copy; 2019, 2020 Matthias Riegler,
+                <a href="/impressum.html">Impressum</a>
+              </div>
+            </i-column>
+          </i-row>
+        </i-container>
+      </i-layout-footer>
+    </i-layout>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app"
+};
 </script>
 
-<style>
+<style lang="css">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 900px;
+}
+
+.image {
+  width: 18%;
+  height: auto;
+  border-radius: 50% !important;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 import Vue from 'vue'
+import '@inkline/inkline/dist/inkline.css';
+import Inkline from '@inkline/inkline';
+import { library } from '@fortawesome/fontawesome-svg-core'
+// import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faGithubAlt, faLinkedinIn, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import App from './App.vue'
 
+library.add(faEnvelope)
+library.add(faGithubAlt)
+library.add(faLinkedinIn)
+library.add(faTwitter)
+
 Vue.config.productionTip = false
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.use(Inkline);
 
 new Vue({
   render: h => h(App),
